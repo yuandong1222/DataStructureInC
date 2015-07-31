43cacde59e482001a7b69e0af55006874aee7c3c
Dong Yuan <yuandong1222@gmail.com>
2015-07-31 16:21:50 +0800

    Finish trunk/tree
    Signed-off-by: Dong Yuan <yuandong1222@gmail.com>

--- trunk/src/tree/child_sibling_tree.c
+++ trunk/src/tree/child_sibling_tree.c
@@ -11,246 +11,46 @@
 #include "include/common.h"
 #include "include/tree/child_sibling_tree.h"
 
-int get_number(char* string, int start_index, int end_index) {
-	int value = 0;
-	int i = string[start_index] == '-' ? start_index + 1 : start_index;
-	for (i = i; i <= end_index; i++) {
-		value = value * 10 + string[i] - '0';
-	}
-	return string[start_index] == '-' ? value * -1 : value;
-}
-void build_definition_array(char* definition, int *length, int **array) {
-	int index = 0;
-	*length = 0;
-	while (definition[index] != '\0') {
-		if (definition[index] == ';' || definition[index] == ',')
-			(*length)++;
-		index++;
-	}
-
-	*array = (int*) malloc(sizeof(int) * (*length));
-
-	index = 0;
-	int i = 0;
-	while (definition[i] != '\0') {
-
-		if ((definition[i] >= '0' && definition[i] <= '9') || definition[i]
-				== '-') {
-			int number_start = i, number_end = i + 1;
-			while (definition[number_end] >= '0' && definition[number_end]
-					<= '9')
-				number_end++;
-			int value = get_number(definition, number_start, number_end - 1);
-
-			(*array)[index++] = value;
-			i = number_end;
-		}
-		if (definition[i] != '\0')
-			i++;
-	}
-}
-
 Status Initial(TREE_TYPE* tree) {
-	*tree = NULL;
-	return OK;
-}
-TREE_NODE *get_node_by_value(TREE_TYPE tree, ElementType value) {
-	if (tree == NULL)
-		return NULL;
-	if (tree -> value == value)
-		return tree;
-
-	ChildSiblingTreeNode *node;
-	node = get_node_by_value(tree -> first_child, value);
-	if (node != NULL)
-		return node;
-
-	node = get_node_by_value(tree -> next_sibling, value);
-	if (node != NULL)
-		return node;
-
-	return NULL;
 }
 Status Create(TREE_TYPE* tree, char* definition) {
-
-	if (definition == "" || definition == NULL) {
-		return OK;
-	}
-
-	int length, *array;
-	build_definition_array(definition, &length, &array);
-
-	int i = 0;
-	while (i < length) {
-
-		if (array[i + 1] == -1) {
-			(*tree) = (TREE_TYPE) malloc(sizeof(ChildSiblingTreeNode));
-			(*tree) -> first_child = NULL;
-			(*tree) -> next_sibling = NULL;
-			(*tree) -> value = array[i];
-			i += 2;
-			continue;
-		}
-
-		int node_value = array[i];
-		int parent_index = array[i + 1];
-		int parent_value = array[parent_index * 2];
-		int order = 1, j = 0;
-		for (j = 0; j < i; j += 2) {
-			if (array[j + 1] == parent_index)
-				order++;
-		}
-		TREE_NODE *node = (TREE_NODE*) malloc(sizeof(TREE_NODE));
-		node -> value = node_value;
-		node -> first_child = NULL;
-		node -> next_sibling = NULL;
-		TREE_NODE *parent = get_node_by_value(*tree, parent_value);
-		if (order == 1) {
-			parent -> first_child = node;
-		} else {
-			j = 1;
-			TREE_NODE *child = parent -> first_child;
-			while (j < order - 1) {
-				child = child -> next_sibling;
-				j++;
-			}
-			node -> next_sibling = child -> next_sibling;
-			child -> next_sibling = node;
-		}
-
-		i += 2;
-	}
-
-	free(array);
-	array = NULL;
-
-	return OK;
 }
 
 Status Destory(TREE_TYPE* tree) {
-	if (*tree == NULL)
-		return OK;
-
-	Destory(&((*tree) -> first_child));
-	Destory(&((*tree) -> next_sibling));
-
-	free(*tree);
-	*tree = NULL;
-
-	return OK;
 }
 
 Status Clear(TREE_TYPE* tree) {
-	return Destory(tree);
 }
 
 Status Empty(TREE_TYPE tree) {
-	return tree == NULL;
 }
 
 int Depth(TREE_TYPE tree) {
-	if (tree == NULL)
-		return 0;
-
-	int depth = 0, child_depth = 0;
-	TREE_NODE *child = tree -> first_child;
-	while (child) {
-		child_depth = Depth(child);
-		if (child_depth > depth)
-			depth = child_depth;
-		child = child -> next_sibling;
-	}
-	return depth + 1;
 }
 
 TREE_NODE* Root(TREE_TYPE tree) {
-	return tree;
 }
 
 ElementType Value(TREE_TYPE tree, TREE_NODE *node) {
-	return node == NULL ? 0 : node -> value;
 }
 
 Status Assign(TREE_TYPE* tree, TREE_NODE *node, ElementType value) {
-	node -> value = value;
-	return OK;
 }
 TREE_NODE* Parent(TREE_TYPE tree, TREE_NODE* node) {
-
-	if (tree == NULL || tree == node)
-		return NULL;
-	TREE_NODE *child = tree -> first_child;
-	while (child) {
-		if (child == node)
-			return tree;
-		TREE_NODE *parent = Parent(child, node);
-		if (parent != NULL)
-			return parent;
-
-		child = child -> next_sibling;
-	}
-
-	return NULL;
 }
 
 TREE_NODE* LeftChild(TREE_TYPE tree, TREE_NODE* node) {
-	return node -> first_child;
 }
 
 TREE_NODE* RightSibling(TREE_TYPE tree, TREE_NODE* node) {
-	return node -> next_sibling;
 }
 
 Status InsertChild(TREE_TYPE tree, TREE_NODE* parent, int position,
 		ElementType value) {
-	TREE_NODE *node = (TREE_NODE*) malloc(sizeof(TREE_NODE));
-	node -> value = value;
-	node -> first_child = NULL;
-	node -> next_sibling = NULL;
-
-	if (position == 1) {
-		node -> next_sibling = parent -> first_child;
-		parent -> first_child = node;
-	} else {
-		int order = 1;
-		TREE_NODE *child = parent -> first_child;
-		while (order < position - 1) {
-			child = child -> next_sibling;
-			order++;
-		}
-		node -> next_sibling = child -> next_sibling;
-		child -> next_sibling = node;
-	}
-	return OK;
 }
 
 Status DeleteChild(TREE_TYPE tree, TREE_NODE* node, int position) {
-	if (position == 1) {
-		Destory(&(node -> first_child));
-	} else {
-		int order = 1;
-		TREE_NODE *child = node -> first_child;
-		while (order < position - 1) {
-			child = child -> next_sibling;
-			order++;
-		}
-		TREE_NODE *n = child -> next_sibling;
-		child -> next_sibling = child -> next_sibling -> next_sibling;
-
-		n -> next_sibling = NULL;
-		Destory(&n);
-	}
-	return OK;
 }
 
 Status Traverse(TREE_TYPE tree, Status(*visit)(ElementType*)) {
-	if (tree == NULL)
-	return OK;
-
-	visit(&(tree -> value));
-
-	Traverse(tree -> first_child, visit);
-	Traverse(tree -> next_sibling, visit);
-
-	return OK;
 }
--- trunk/src/tree/sequence_binary_tree.c
+++ trunk/src/tree/sequence_binary_tree.c
@@ -12,270 +12,81 @@
 #include "include/tree/sequence_binary_tree.h"
 #include "include/tree/binary_tree.h"
 
-int get_number(char* string, int start_index, int end_index) {
-	int value = 0;
-	int i = start_index;
-	for (i = start_index; i <= end_index; i++) {
-		value = value * 10 + string[i] - '0';
-	}
-	return value;
-}
-
 Status Initial(BINARY_TREE_TYPE* tree) {
-	*tree = (ElementType*) malloc(sizeof(ElementType) * MAX_NODE);
-	return Clear(tree);
 }
 
 Status Destory(BINARY_TREE_TYPE* tree) {
-	free(*tree);
-	*tree = NULL;
-	return OK;
 }
 
 Status Create(BINARY_TREE_TYPE* tree, char* definition) {
-
-	int i = 0, node_index = 0;
-	while (definition[i] != '\0') {
-
-		if (definition[i] == ',') {
-			(*tree)[node_index] = Nil;
-			node_index++;
-		}
-
-		if (definition[i] >= '0' && definition[i] <= '9') {
-			int number_start = i, number_end = i + 1;
-			while (definition[number_end] != ',' && definition[number_end]
-					!= ' ' && definition[number_end] != '\0')
-				number_end++;
-			int value = get_number(definition, number_start, number_end - 1);
-
-			//еп╣х
-			(*tree)[node_index] = value;
-
-			//next
-			node_index++;
-			while (definition[number_end] != ',' && definition[number_end]
-					!= '\0')
-				number_end++;
-			i = number_end;
-		}
-		if (definition[i] != '\0')
-			i++;
-	}
-
-	return OK;
 }
 
 Status Clear(BINARY_TREE_TYPE* tree) {
-	int i = 0;
-	for (i = 0; i < MAX_NODE; i++) {
-		(*tree)[i] = Nil;
-	}
-	return OK;
 }
 
 Status Empty(BINARY_TREE_TYPE tree) {
-	return *tree == Nil;
 }
 
 Status Depth(BINARY_TREE_TYPE tree) {
-	if (Empty(tree))
-		return 0;
-
-	int n = MAX_NODE - 1;
-	while (tree[n] == Nil)
-		n--;
-
-	int depth = 1, max_node = 1;
-	while (max_node < n + 2) {
-		max_node *= 2;
-		depth++;
-	}
-	return depth - 1;
 }
 
 BINARY_TREE_NODE* Root(BINARY_TREE_TYPE tree) {
-	return *tree == Nil ? NULL : tree;
 }
 
 ElementType Value(BINARY_TREE_TYPE tree, Position position) {
-	if (position.level == 1) {
-		return *tree == Nil ? 0 : *tree;
-	}
-
-	int offset = 1, i = 0, temp = 1;
-	for (i = 0; i < position.level - 2; i++) {
-		temp *= 2;
-		offset += temp;
-	}
-	offset += position.order - 1;
-
-	return tree[offset] == Nil ? 0 : tree[offset];
 }
 
 Status Assign(BINARY_TREE_TYPE* tree, Position position, ElementType value) {
-	if (position.level == 1) {
-		**tree = value;
-		return OK;
-	}
-
-	int offset = 1, i = 0, temp = 1;
-	for (i = 0; i < position.level - 2; i++) {
-		temp *= 2;
-		offset += temp;
-	}
-	offset += position.order - 1;
-
-	if ((*tree)[offset] == Nil)
-		return ERROR;
-
-	(*tree)[offset] = value;
-	return OK;
 }
 
 BINARY_TREE_NODE* Parent(BINARY_TREE_TYPE tree, BINARY_TREE_NODE* node) {
-
-	if (node == tree)
-		return NULL;
-
-	int node_index = node - tree + 1;
-	int parent_offset = node_index / 2;
-
-	return tree + parent_offset - 1;
 }
 
 BINARY_TREE_NODE* getChildPointer(BINARY_TREE_TYPE tree,
 		BINARY_TREE_NODE* node, int left_or_right) {
-
-	int node_index = node - tree;
-	return tree + node_index * 2 + 1 + left_or_right;
 }
 BINARY_TREE_NODE* getChild(BINARY_TREE_TYPE tree, BINARY_TREE_NODE* node,
 		int left_or_right) {
-	int* left_child_pointer = getChildPointer(tree, node, left_or_right);
-	return *left_child_pointer == Nil ? NULL : left_child_pointer;
 }
 
 BINARY_TREE_NODE* LeftChild(BINARY_TREE_TYPE tree, BINARY_TREE_NODE* node) {
-	return getChild(tree, node, 0);
 }
 
 BINARY_TREE_NODE* RightChild(BINARY_TREE_TYPE tree, BINARY_TREE_NODE* node) {
-	return getChild(tree, node, 1);
 }
 
 BINARY_TREE_NODE* LeftSibling(BINARY_TREE_TYPE tree, BINARY_TREE_NODE* node) {
-	if (tree == node)
-		return NULL;
-
-	int offset = node - tree;
-	if (offset % 2 == 1)
-		return NULL;
-
-	int* left_sibling_pointer = node - 1;
-
-	return *left_sibling_pointer == Nil ? NULL : left_sibling_pointer;
 }
 
 BINARY_TREE_NODE* RightSibling(BINARY_TREE_TYPE tree, BINARY_TREE_NODE* node) {
-	int offset = node - tree;
-	if (offset % 2 == 0)
-		return NULL;
-
-	int* right_sibling_pointer = node + 1;
-
-	return *right_sibling_pointer == Nil ? NULL : right_sibling_pointer;
 }
 
 Status InsertChild(BINARY_TREE_TYPE tree, BINARY_TREE_NODE* parent,
 		enum LeftOrRight left_or_right, ElementType value) {
-	int left_or_right_value = left_or_right == Left ? 0 : 1;
-	int* child_pointer = getChildPointer(tree, parent, left_or_right_value);
-
-	if (*child_pointer != Nil)
-		return ERROR;
-
-	*child_pointer = value;
-	return OK;
 }
 
 Status DeleteChild(BINARY_TREE_TYPE tree, BINARY_TREE_NODE* node,
 		enum LeftOrRight left_or_right) {
-
-	int left_or_right_value = left_or_right == Left ? 0 : 1;
-	int* child_pointer = getChildPointer(tree, node, left_or_right_value);
-
-	if (*child_pointer == Nil)
-		return OK;
-
-	DeleteChild(tree, child_pointer, Left);
-	DeleteChild(tree, child_pointer, Right);
-	*child_pointer = Nil;
-
-	return OK;
 }
 
 void PreOrderTraverseAction(BINARY_TREE_TYPE tree, BINARY_TREE_NODE* parent,
 		Status(*visit)(ElementType*)) {
-
-	int* left_child_pointer = getChildPointer(tree, parent, 0);
-	int* right_child_pointer = getChildPointer(tree, parent, 1);
-
-	visit(parent);
-	if (*left_child_pointer != Nil)
-		PreOrderTraverseAction(tree, left_child_pointer, visit);
-	if (*right_child_pointer != Nil)
-		PreOrderTraverseAction(tree, right_child_pointer, visit);
 }
 
 Status PreOrderTraverse(BINARY_TREE_TYPE tree, Status(*visit)(ElementType*)) {
-	PreOrderTraverseAction(tree, tree, visit);
-	return OK;
 }
 
 void InOrderTraverseAction(BINARY_TREE_TYPE tree, BINARY_TREE_NODE* parent,
 		Status(*visit)(ElementType*)) {
-
-	int* left_child_pointer = getChildPointer(tree, parent, 0);
-	int* right_child_pointer = getChildPointer(tree, parent, 1);
-
-	if (*left_child_pointer != Nil)
-		InOrderTraverseAction(tree, left_child_pointer, visit);
-
-	visit(parent);
-
-	if (*right_child_pointer != Nil)
-		InOrderTraverseAction(tree, right_child_pointer, visit);
 }
 Status InOrderTraverse(BINARY_TREE_TYPE tree, Status(*visit)(ElementType*)) {
-	InOrderTraverseAction(tree, tree, visit);
-	return OK;
 }
 
 void PostOrderTraverseAction(BINARY_TREE_TYPE tree, BINARY_TREE_NODE* parent,
 		Status(*visit)(ElementType*)) {
-
-	int* left_child_pointer = getChildPointer(tree, parent, 0);
-	int* right_child_pointer = getChildPointer(tree, parent, 1);
-
-	if (*left_child_pointer != Nil)
-		PostOrderTraverseAction(tree, left_child_pointer, visit);
-	if (*right_child_pointer != Nil)
-		PostOrderTraverseAction(tree, right_child_pointer, visit);
-
-	visit(parent);
 }
 Status PostOrderTraverse(BINARY_TREE_TYPE tree, Status(*visit)(ElementType*)) {
-	PostOrderTraverseAction(tree, tree, visit);
-	return OK;
 }
 
 Status LevelOrderTraverse(BINARY_TREE_TYPE tree, Status(*visit)(ElementType*)) {
-	int i = 0, length = MAX_NODE - 1;
-	while (tree[length] == Nil)
-		length--;
-	for (i = 0; i <= length; i++)
-		if (tree[i] != Nil)
-			visit(tree + i);
-	return OK;
 }
